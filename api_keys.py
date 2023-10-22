071c72999932c1bba6da51f722b41157a3bf10ca
jkalsi1997 <141664737+jkalsi1997@users.noreply.github.com>
2023-10-22 16:30:42 +0100

    Update api_keys.py

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "ae8d51dca836751858749174d78a0a01"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "8b168311cb2c46cabaa74d5774ac7c7f"
